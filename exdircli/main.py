637c8f836e1f72ce526bd682d947c72b82816f9f
Svenn-Arne Dragly <s@dragly.com>
2017-03-27 14:43:41 +0200

    Using classes to abstract commands

--- exdircli/main.py
+++ exdircli/main.py
@@ -29,79 +29,140 @@ def verify_inside(directory):
         sys.exit(1)
 
 
-def list_(args):
-    directory = args.object or '.'
-    verify_inside(directory)
-    try:
-        obj = exdir.core.open_object(directory)
-    except KeyError:
-        return 0
-    if isinstance(obj, exdir.core.Group):
-        for sub in obj.values():
-            if isinstance(sub, exdir.core.Group):
-                cprint(sub.object_name, 'blue', attrs=['bold'])
-            elif isinstance(sub, exdir.core.Raw):
-                cprint(sub.object_name, 'yellow', attrs=['bold'])
-            else:
-                print(sub.object_name)
-        return 0
-    elif isinstance(obj, exdir.core.Dataset):
-        print(obj.object_name)
-        return 0
-    else:
-        print("Cannot list object of this type:", obj)
-        return 1
+class ListCommand:
+    def command(args):
+        directory = args.object or "."
+        verify_inside(directory)
+        try:
+            obj = exdir.core.open_object(directory)
+        except KeyError:
+            return 0
+        if isinstance(obj, exdir.core.Group):
+            for sub in obj.values():
+                if isinstance(sub, exdir.core.Group):
+                    cprint(sub.object_name, "blue", attrs=["bold"])
+                elif isinstance(sub, exdir.core.Raw):
+                    cprint(sub.object_name, "yellow", attrs=["bold"])
+                else:
+                    print(sub.object_name)
+            return 0
+        elif isinstance(obj, exdir.core.Dataset):
+            print(obj.object_name)
+            return 0
+        else:
+            print("Cannot list object of this type:", obj)
+            return 1
+            
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            "list", 
+            aliases=["ls"],
+            description="List contents",
+            help="list contents of object"
+        )
+        parser.add_argument(
+            "object", 
+            help="name of object to list contents of, should be File or Group", 
+            default=None, 
+            nargs="?"
+        )
+        return parser
+    
         
+class InfoCommand:
+    def command(args):
+        directory = args.object or "."
+        verify_inside(directory)
+        obj = exdir.core.open_object(directory)
+        if isinstance(obj, exdir.core.Group):
+            print("Group:", obj.object_name)
+            print("Item count:", len(list(obj.keys())))
+            return 0
+        if isinstance(obj, exdir.core.Dataset):
+            print("Dataset:", obj.object_name)
+            return 0
+        if isinstance(obj, exdir.core.Raw):
+            print("Raw:", obj.object_name)
+            return 0
+            
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            "info",
+            description="Show info about object",
+            help="show short info about object"
+        )
+        parser.add_argument("object", help="object to list info about", default=None, nargs="?")
+        return parser
 
-def info(args):
-    directory = args.object or '.'
-    verify_inside(directory)
-    obj = exdir.core.open_object(directory)
-    if isinstance(obj, exdir.core.Group):
-        print("Group:", obj.object_name)
-        print("Item count:", len(list(obj.keys())))
-        return 0
-    if isinstance(obj, exdir.core.Dataset):
-        print("Dataset:", obj.object_name)
-        return 0
-    if isinstance(obj, exdir.core.Raw):
-        print("Raw:", obj.object_name)
-        return 0
+
+class ShowCommand:
+    def command(args):
+        directory = args.object or "."
+        verify_inside(directory)
+        obj = exdir.core.open_object(directory)
+        
+        if args.all:
+            import numpy as np
+            np.set_printoptions(threshold=np.inf)        
+        
+        if isinstance(obj, exdir.core.File):
+            print("__root__")
+            print("Type: File")
+            print("Item count:", len(list(obj.keys())))
+            print("Items:")
+            ListCommand.command(args)
+            return 0
+        if isinstance(obj, exdir.core.Group):
+            print(obj.object_name)
+            print("Type: Group")
+            print("Name:", obj.name)
+            print("Item count:", len(list(obj.keys())))
+            print("Items:")
+            ListCommand.command(args)
+            return 0
+        if isinstance(obj, exdir.core.Dataset):
+            print(obj.object_name)
+            print("Type: Dataset")
+            print("Name:", obj.name)
+            print("Shape:", obj.shape)
+            print("Data:")
+            print(obj.data)
+            return 0
+        if isinstance(obj, exdir.core.Raw):
+            print(obj.object_name)
+            print("Type: Raw")
+            print("Name:", obj.name)
+            return 0
+            
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            "show",
+            description="Show object contents",
+            help="show object contents"
+        )
+        parser.add_argument("object", help="object to show", default=None, nargs="?")
+        parser.add_argument("--all", "-a", action="store_true", help="show all data in datasets")
+        return parser
 
 
-def show(args):
-    directory = args.object or '.'
-    verify_inside(directory)
-    obj = exdir.core.open_object(directory)
-    
-    if args.all:
-        import numpy as np
-        np.set_printoptions(threshold=np.inf)        
-    
-    if isinstance(obj, exdir.core.File):
-        print("__root__")
-        print("Type: File")
-        print("Item count:", len(list(obj.keys())))
-        return 0
-    if isinstance(obj, exdir.core.Group):
-        print(obj.object_name)
-        print("Type: Group")
-        print("Name:", obj.name)
-        print("Item count:", len(list(obj.keys())))
-        return 0
-    if isinstance(obj, exdir.core.Dataset):
-        print(obj.object_name)
-        print("Type: Dataset")
-        print("Name:", obj.name)
-        print("Shape:", obj.shape)
-        print("Data:")
-        print(obj.data)
-        return 0
-    if isinstance(obj, exdir.core.Raw):
-        print(obj.object_name)
-        print("Type: Raw")
-        print("Name:", obj.name)
-        return 0
+class CreateGroupCommand:
+    def command(args):
+        exdir.create_group(args.name)
+        
+    def add_parser(subparsers):
+        parser = subparsers.add_parser(
+            "create",
+            aliases=["mkgrp"],
+            description="Create group",
+            help="create group"
+        )
+        parser.add_argument(
+            "name", 
+            help="name(s) of the group(s) to create",
+            default=None,
+            nargs="+"
+        )
+        return parser
 
 
 class _HelpAction(argparse._HelpAction):
@@ -109,16 +170,16 @@ class _HelpAction(argparse._HelpAction):
     def __call__(self, parser, namespace, values, option_string=None):
         parser.print_help()
 
-        # retrieve sub_parsers from parser
-        sub_parsers_actions = [
+        # retrieve subparsers from parser
+        subparsers_actions = [
             action for action in parser._actions
-            if isinstance(action, argparse._sub_parsersAction)]
+            if isinstance(action, argparse._subparsersAction)]
         # there will probably only be one subparser_action,
         # but better save than sorry
         print("\nThese are available exdir commands:\n")
-        for sub_parsers_action in sub_parsers_actions:
-            # get all sub_parsers and print help
-            for choice, subparser in sub_parsers_action.choices.items():
+        for subparsers_action in subparsers_actions:
+            # get all subparsers and print help
+            for choice, subparser in subparsers_action.choices.items():
                 print("\t{}\t\t{}".format(choice, subparser.description))
                 # print(subparser.format_help())
 
@@ -127,50 +188,27 @@ class _HelpAction(argparse._HelpAction):
 
 def main():
     # parser = argparse.ArgumentParser(add_help=False)
-    # parser.add_argument('--help', action=_HelpAction, help='show this help message and exit')
+    # parser.add_argument("--help", action=_HelpAction, help="show this help message and exit")
     parser = argparse.ArgumentParser()
 
-    sub_parsers = parser.add_subparsers(metavar="command", dest="cmd")
-    sub_parsers.required = True
+    subparsers = parser.add_subparsers(metavar="command", dest="cmd")
+    subparsers.required = True
 
-    list_parser = sub_parsers.add_parser(
-        'list', 
-        aliases=['ls'],
-        description="List contents",
-        help="list contents of object"
-    )
-    list_parser.add_argument('object', help="name of object to list contents of, should be File or Group", default=None, nargs='?')
-    list_parser.set_defaults(func=list_)
-
-    view_parser = sub_parsers.add_parser(
-        'show',
-        description="Show object contents",
-        help="show object contents"
-    )
-    view_parser.add_argument('object', help="object to show", default=None, nargs='?')
-    view_parser.add_argument('--all', '-a', action='store_true', help="show all data in datasets")
-    view_parser.set_defaults(func=show)
-
-    info_parser = sub_parsers.add_parser(
-        'info',
-        description="Show info about object",
-        help="show short info about object"
-    )
-    info_parser.add_argument('object', help="object to list info about", default=None, nargs='?')
-    info_parser.set_defaults(func=info)
-
-    info_parser = sub_parsers.add_parser(
-        'create',
-        description="Create object",
-        help="create object"
-    )
-    info_parser.add_argument('object', help="object to list info about", default=None, nargs='?')
-    info_parser.set_defaults(func=info)
+    commands = [
+        ListCommand,
+        ShowCommand,
+        InfoCommand,
+        CreateGroupCommand
+    ]
+    
+    for command in commands:
+        subparser = command.add_parser(subparsers)
+        subparser.set_defaults(func=command.command)
     
     def completer(prefix, **kwargs):
-            return [i for i in list(sub_parsers.choices) + find_commands() if i.startswith(prefix)]
+        return [i for i in list(subparsers.choices) + find_commands() if i.startswith(prefix)]
     
-    sub_parsers.completer = completer
+    subparsers.completer = completer
 
     args = parser.parse_args()
 
