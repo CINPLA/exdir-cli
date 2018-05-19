5628f6396ca269e884ac77e2b4b400acfea5b40a
Svenn-Arne Dragly <s@dragly.com>
2018-05-19 15:58:13 +0200

    git subrepo pull libs/travis-conda-scripts
    subrepo:
      subdir:   "libs/travis-conda-scripts"
      merged:   "02e37cc"
    upstream:
      origin:   "git@github.com:CINPLA/travis-conda-scripts.git"
      branch:   "master"
      commit:   "02e37cc"
    git-subrepo:
      version:  "0.3.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "3525fd9"

--- libs/travis-conda-scripts/scripts/config.sh
+++ libs/travis-conda-scripts/scripts/config.sh
@@ -5,11 +5,13 @@ export GIT_TAG=$(git describe --always --tags --abbrev=0)
 export TAG_AND_TRAVIS_BUILD=${GIT_TAG}_b${TRAVIS_BUILD_NUMBER}
 export TAG_TRAVIS_BUILD_AND_COMMIT=${TAG_AND_TRAVIS_BUILD}_g${TRAVIS_COMMIT}
 export CONDA_BLD_PATH=/tmp/conda-bld
-export PACKAGE=$(conda build . --output --python "$TRAVIS_PYTHON_VERSION")
+export PACKAGE=$(conda build . --output --old-build-string --python "$TRAVIS_PYTHON_VERSION" 2> conda-errors.log | tail -1)
 export EXTRA_CONDA_CHANNELS="-c defaults -c conda-forge -c cinpla"
+export GIT_STRING=${GIT_TAG}
 if [ -z $TRAVIS_TAG ]; then
     echo "INFO: No TRAVIS_TAG found, adding dev channel"
     export EXTRA_CONDA_CHANNELS="$EXTRA_CONDA_CHANNELS -c cinpla/label/dev"
+    export GIT_STRING="${GIT_TAG}_latest"
 fi
 echo PATH $PATH
 echo GIT_DESCRIBE $GIT_DESCRIBE
@@ -19,3 +21,5 @@ echo TAG_AND_TRAVIS_BUILD $TAG_AND_TRAVIS_BUILD
 echo TAG_TRAVIS_BUILD_AND_COMMIT $TAG_TRAVIS_BUILD_AND_COMMIT
 echo PACKAGE $PACKAGE
 echo CONDA_CHANNELS $EXTRA_CONDA_CHANNELS
+echo conda-errors:
+cat conda-errors.log
